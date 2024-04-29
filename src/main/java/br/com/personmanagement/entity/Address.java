7c842ac99686f21f83007e7f4b747462bbc14fd8
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-04-29 19:15:06 -0300

    Adicionar entidade Address

--- src/main/java/br/com/personmanagement/entity/Address.java
+++ src/main/java/br/com/personmanagement/entity/Address.java
@@ -1,4 +1,37 @@
 package br.com.personmanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private People person;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String city;
+
+
+
 }
