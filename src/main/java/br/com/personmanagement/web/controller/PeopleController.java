5bcc3de1732265bf588c9e4d01598f4585b8d7ec
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-05-06 10:22:19 -0300

    tratando exceção customizada na camada de peopleController

--- src/main/java/br/com/personmanagement/web/controller/PeopleController.java
+++ src/main/java/br/com/personmanagement/web/controller/PeopleController.java
@@ -6,7 +6,10 @@ import br.com.personmanagement.domain.entity.People;
 import br.com.personmanagement.domain.service.PeopleService;
 import br.com.personmanagement.infrastructure.assembler.PeopleConverter;
 import br.com.personmanagement.infrastructure.repository.PeopleRepository;
-import jakarta.validation.Valid;
+import javax.validation.Valid;
+
+import br.com.personmanagement.web.exception.BusinessException;
+import br.com.personmanagement.web.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -43,13 +46,22 @@ public class PeopleController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PeopleResponseDTO createPeople(@RequestBody @Valid PeopleRequestDTO peopleRequestDTO) {
-        return peopleConverter.toDto(peopleService.create(peopleRequestDTO));
+        try {
+            return peopleConverter.toDto(peopleService.create(peopleRequestDTO));
+        }
+        catch (ResourceNotFoundException e){
+                throw new BusinessException(e.getMessage());
+        }
     }
-
 
     @PutMapping("/{peopleId}")
     public PeopleResponseDTO update(@PathVariable Long peopleId, @RequestBody @Valid PeopleRequestDTO peopleRequestDTO) {
-        People updatedPeople = peopleService.update(peopleId, peopleRequestDTO);
-        return peopleConverter.toDto(updatedPeople);
+        try {
+            People updatedPeople = peopleService.update(peopleId, peopleRequestDTO);
+            return peopleConverter.toDto(updatedPeople);
+        }
+        catch (ResourceNotFoundException e){
+            throw new BusinessException(e.getMessage());
+        }
     }
 }
